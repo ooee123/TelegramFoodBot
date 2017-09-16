ce6cd6f6539259d34f86918ab53836e242a3c8a0
Kevin Ly <ooee123@gmail.com>
2017-09-16 00:40:32 -0700

    Modular chatroom

--- bot.py
+++ bot.py
@@ -1,40 +1,34 @@
 #!/usr/bin/python3
 import sys
-import requests
 import json
-
-global baseUrl
+from ChatroomConnection import ChatroomConnection
+from Config import Config
 
 def main():
     if len(sys.argv) < 2:
         print("Usage {0} <bot_token>".format(sys.argv[0]))
         sys.exit(-1)
 
-    global baseUrl
-    baseUrl = "https://api.telegram.org/bot{token}".format(token=sys.argv[1])
-
+    token = sys.argv[1]
     chatroom = int(open("chatroom_id").read())
-    response = sendSticker(chatroom, "CAADAgAD2QMAAjq5FQLlmqkIqp10lQI")
-    printJSON(response)
 
-    response = postAPI("getUpdates")
-    printJSON(response)
+    connection = ChatroomConnection(token, chatroom)
+    config = Config("stickers.json")
+    print(config.getLastOffset())
+    newMessages = connection.getUpdates(offset = config.getLastOffset())["results"]
 
-def getAPI(api, params=None):
-    return requests.get("{baseUrl}/{api}".format(baseUrl=baseUrl, api=api), params=params).json()
-   
-def postAPI(api, params=None):
-    headers = {'content-type': 'application/json'}
-    return requests.post("{baseUrl}/{api}".format(baseUrl=baseUrl, api=api), params=params, headers=headers).json()
+    for newMessage in newMessages:
+        
+
+    printJSON(newMessages)
+    config.setLastOffset(getUpdateID(newMessages[-1]) + 1)
+
+    connection.sendMessage("Hello")
 
 def printJSON(message):
     print(json.dumps(message, indent=4, sort_keys=True, separators=(',', ': ')))
 
-def sendSticker(chat_id, sticker, params={}):
-    chat_id = int(chat_id)
-    sticker = str(sticker)
-    params['chat_id'] = chat_id
-    params['sticker'] = sticker 
-    getAPI("sendSticker", params)
+def getUpdateID(message):
+    return int(message["update_id"])
 
 main()
